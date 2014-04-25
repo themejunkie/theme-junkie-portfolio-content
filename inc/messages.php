e39b74f7c4e71a822a8761f9f0e3ce2e37f428b9
Satrya <satrya@satrya.me>
2014-04-25 16:15:05 +0700

    Change updated messages method.

--- inc/messages.php
+++ inc/messages.php
@@ -21,44 +21,23 @@ add_filter( 'post_updated_messages', 'tjpc_updated_messages' );
  * @return array  Amended post update messages with new CPT update messages.
  */
 function tjpc_updated_messages( $messages ) {
-	$post             = get_post();
-	$post_type        = get_post_type( $post );
-	$post_type_object = get_post_type_object( $post_type );
+	global $post, $post_ID;
 
 	$messages['portfolio'] = array(
-		0  => '',
-		1  => __( 'Project updated.', 'tjpc' ),
-		2  => __( 'Custom field updated.', 'tjpc' ),
-		3  => __( 'Custom field deleted.', 'tjpc' ),
-		4  => __( 'Project updated.', 'tjpc' ),
-		/* translators: %s: date and time of the revision */
-		5  => isset( $_GET['revision'] ) ? sprintf( __( 'Project restored to revision from %s', 'tjpc' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-		6  => __( 'Project published.', 'tjpc' ),
-		7  => __( 'Project saved.', 'tjpc' ),
-		8  => __( 'Project submitted.', 'tjpc' ),
-		9  => sprintf(
-			__( 'Project scheduled for: <strong>%1$s</strong>.', 'tjpc' ),
-			// translators: Publish box date format, see http://php.net/date
-			date_i18n( __( 'M j, Y @ G:i', 'tjpc' ), strtotime( $post->post_date ) )
-		),
-		10 => __( 'Project draft updated.', 'tjpc' ),
+		0 => '',
+		1 => sprintf( __( 'Project updated. <a href="%s">View Project</a>', 'tjtc' ), esc_url( get_permalink( $post_ID ) ) ),
+		2 => __( 'Custom field updated.', 'tjtc' ),
+		3 => __( 'Custom field deleted.', 'tjtc' ),
+		4 => __( 'Project updated.', 'tjtc' ),
+		5 => isset( $_GET['revision'] ) ? sprintf( __( 'Project restored to revision from %s', 'tjtc' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6 => sprintf( __( 'Project published. <a href="%s">View It</a>', 'tjtc' ), esc_url( get_permalink( $post_ID ) ) ),
+		7 => __( 'Project saved.', 'tjtc' ),
+		8 => sprintf( __( 'Project submitted. <a target="_blank" href="%s">Preview Project</a>', 'tjtc' ), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+		9 => sprintf( __( 'Project scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview Project</a>', 'tjtc' ),
+		  // translators: Publish box date format, see http://php.net/date
+		  date_i18n( __( 'M j, Y @ G:i', 'tjtc' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ) ),
+		10 => sprintf( __( 'Project draft updated. <a target="_blank" href="%s">Preview Project</a>', 'tjtc' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
 	);
-
-	if ( $post_type_object->publicly_queryable ) {
-
-		$permalink = get_permalink( $post->ID );
-
-		$view_link = sprintf( ' <a href="%s">%s</a>', esc_url( $permalink ), __( 'View project', 'tjpc' ) );
-		$messages[ $post_type ][1] .= $view_link;
-		$messages[ $post_type ][6] .= $view_link;
-		$messages[ $post_type ][9] .= $view_link;
-
-		$preview_permalink = add_query_arg( 'preview', 'true', $permalink );
-		$preview_link = sprintf( ' <a target="_blank" href="%s">%s</a>', esc_url( $preview_permalink ), __( 'Preview project', 'tjpc' ) );
-		$messages[ $post_type ][8]  .= $preview_link;
-		$messages[ $post_type ][10] .= $preview_link;
-
-	}
 
 	return $messages;
 }
